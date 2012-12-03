70dfa194cffc06ddf50ae4dcd41b0e998e1a8715
HawaiiHealingWorks <matthew@hawaiihealingworks.com>
2012-12-02 21:16:20 -1000

    First Commit

---
+++ test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '425f0eacf33e9555efc0127b030d6c122570e582c4960f05e0d6b8057aca29b8b861ba1c17a732971bb974a1bfdbe68fcca0fa468d49996190b73fb105a1a0c8'
